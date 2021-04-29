931d888c88fa9274d53a3507e5fe0b2700b8ee55
FloJoeHD <fpiminger@gmail.com>
2021-04-29 21:33:35 +0200

    added quantifiers, still some adaptions needed

--- encoding_gen.py
+++ encoding_gen.py
@@ -1,15 +1,42 @@
 # this should only be a short test file for the Sim game
 
 # short test of how to write to a file, this then should be done to generate the input for limboole
-list_of_segments = ["% init board with all positions to empty\n"]
+
+# at the beginning we want to write the alternating quantifiers to the file --------------------------------------------
+
+# a single colored line is represented like this: green$0_0.1
+list_of_segments = ["% first, quantifiers are written\n? ( "]
+# step 0
+for i in range(0, 5):
+    for j in range(i + 1, 6):
+        list_of_segments.append("green$0_" + str(i) + "." + str(j) + " |\n")
+        list_of_segments.append("red$0_" + str(i) + "." + str(j) + " |\n")
+
+list_of_segments[-1] = list_of_segments[-1][:-3] + " )\n"  # change last element
+for s in range(1, 11):  # run through all states from 1 to 10 (included)
+    if s % 2 != 0:  # every odd state, we use an existential quantifier '?'
+        list_of_segments.append("? (")
+    else:  # every even state, we use a for all quantifier '#'
+        list_of_segments.append("# (")
+    for i in range(0, 5):
+        for j in range(i + 1, 6):
+            list_of_segments.append("green$" + str(s) + "_" + str(i) + "." + str(j) + " |\n")
+            list_of_segments.append("red$" + str(s) + "_" + str(i) + "." + str(j) + " |\n")
+
+    list_of_segments[-1] = list_of_segments[-1][:-3] + " )\n"  # change last element of quantifier part
+
+list_of_segments[-1] = list_of_segments[-1][:-3] + ") &\n"  # change last element
+# ----------------------------------------------------------------------------------------------------------------------
+
+# append init formula --------------------------------------------------------------------------------------------------
+list_of_segments.append("% init board with all positions to empty\n")
 for i in range(0, 5):
     for j in range(i + 1, 6):
         list_of_segments.append("! green$0_" + str(i) + "." + str(j) + " &\n")
         list_of_segments.append("! red$0_" + str(i) + "." + str(j) + " &\n")
+# ----------------------------------------------------------------------------------------------------------------------
 
-#  list_of_segments[-1] = list_of_segments[-1][:-3]  # change last element so that " &\n" is cut off
-# print(list_of_segments)
-
+# append goal state formula --------------------------------------------------------------------------------------------
 list_of_segments.append("% define goal states\n")
 for i in range(0, 4):
     for j in range(i + 1, 6):
@@ -17,8 +44,9 @@ for i in range(0, 4):
             list_of_segments.append("green$10_" + str(i) + "." + str(j) +
                                     " & green$10_" + str(i) + "." + str(y) +
                                     " & green$10_" + str(y) + "." + str(j) + " |\n")
-list_of_segments[-1] = list_of_segments[-1][:-3]
-list_of_segments[-1] = list_of_segments[-1] + " &\n"
+list_of_segments[-1] = list_of_segments[-1][:-3]  # cut off last part since this is the end of the formula
+# ----------------------------------------------------------------------------------------------------------------------
+# TODO: change this section according to complete formula (join together with '&' and '->'; attention to brackets!)
 list_of_segments.append("% define moves of players\n")
 list_of_segments.append("% player green:\n")
 # moves of green
