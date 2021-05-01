040774d6e38413d1410a4696b926c708a9d2c6c5
FloJoeHD <fpiminger@gmail.com>
2021-05-02 00:52:46 +0200

    brackets on moves corrected, goal state changed to red TODO: how far to run with the quantifiers?

--- encoding_gen.py
+++ encoding_gen.py
@@ -10,8 +10,7 @@ for i in range(0, 5):
         list_of_segments.append("? green$0_" + str(i) + "." + str(j) + "\n")
         list_of_segments.append("? red$0_" + str(i) + "." + str(j) + "\n")
 
-#  list_of_segments[-1] = list_of_segments[-1][:-3] + " )\n"  # change last element
-for s in range(1, 11):  # run through all states from 1 to 10 (included)
+for s in range(1, 10):  # TODO: run through all states from 1 to 10 (included) or till 9? can this end with '#'?
 
     for i in range(0, 5):
         for j in range(i + 1, 6):
@@ -24,9 +23,8 @@ for s in range(1, 11):  # run through all states from 1 to 10 (included)
                 list_of_segments.append("# green$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
                 list_of_segments.append("# red$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
 
-    #  list_of_segments[-1] = list_of_segments[-1][:-3] + " )\n"  # change last element of quantifier part
 
-list_of_segments[-1] = list_of_segments[-1][:-3] + "\n"  # change last element
+# list_of_segments[-1] = list_of_segments[-1][:-3] + "\n"  # change last element
 # ----------------------------------------------------------------------------------------------------------------------
 
 # append init formula --------------------------------------------------------------------------------------------------
@@ -38,9 +36,12 @@ for i in range(0, 5):
 # ----------------------------------------------------------------------------------------------------------------------
 
 # moves of players in alternating turns --------------------------------------------------------------------------------
-# TODO: change this section according to complete formula (join together with '&' and '->'; attention to brackets!)
+# moves have brackets around them and also implies has brackets eg. x & y & (a -> b)
 list_of_segments.append("% define moves of players\n")
 for s in range(0, 10):
+    list_of_segments.append("(\n")
+    if s % 2 != 0 and s != 9:  # in the last state we don't need to open another bracket
+        list_of_segments.append("(\n")  # open bracket before '->'
     for i in range(0, 5):
         for j in range(i + 1, 6):
             list_of_segments.append("(\n")
@@ -52,25 +53,25 @@ for s in range(0, 10):
                                                 " <-> green$" + str(s + 1) + "_" + str(k) + "." + str(l) + ") & ")
                         list_of_segments.append("( red$" + str(s) + "_" + str(k) + "." + str(l) +
                                                 " <-> red$" + str(s + 1) + "_" + str(k) + "." + str(l) + ") &\n")
-            if s % 2 == 0:
+            if s % 2 == 0:  # green move
                 list_of_segments.append("! green$" + str(s) + "_" + str(i) + "." + str(j) +
                                         " & ! red$" + str(s) + "_" + str(i) + "." + str(j) +
                                         " & green$" + str(s + 1) + "_" + str(i) + "." + str(j) +
                                         " & ! red$" + str(s + 1) + "_" + str(i) + "." + str(j) + "\n")
-            else:
+            else:  # red move
                 list_of_segments.append("! green$" + str(s) + "_" + str(i) + "." + str(j) +
                                         " & ! red$" + str(s) + "_" + str(i) + "." + str(j) +
                                         " & ! green$" + str(s + 1) + "_" + str(i) + "." + str(j) +
                                         " & red$" + str(s + 1) + "_" + str(i) + "." + str(j) + "\n")
             list_of_segments.append(") | ")
-    if s % 2 == 0:  # TODO: fix brackets here, not sure how to place them around implies ('->')
-        if s == 0:  # at the beginning we don't need to close a bracket
-            list_of_segments[-1] = list_of_segments[-1][:-2] + "&\n% next players move\n("
+    if s % 2 == 0:
+        if s == 0:  # at the beginning we don't need to close an additional bracket
+            list_of_segments[-1] = list_of_segments[-1][:-2] + ") &\n% next players move\n"
         else:
-            list_of_segments[-1] = list_of_segments[-1][:-2] + ") &\n% next players move\n("
+            list_of_segments[-1] = list_of_segments[-1][:-2] + ")) &\n% next players move\n"
     else:
-        list_of_segments[-1] = list_of_segments[-1][:-2] + ") -> (\n"
-list_of_segments[-1] = list_of_segments[-1][:-5] + "&"  # change last element so that the last 5 chars are cut off
+        list_of_segments[-1] = list_of_segments[-1][:-2] + ") ->\n"
+list_of_segments[-1] = list_of_segments[-1][:-3] + "&"  # change last element so that the last 3 chars are cut off
 # ----------------------------------------------------------------------------------------------------------------------
 
 # append goal state formula --------------------------------------------------------------------------------------------
@@ -78,13 +79,13 @@ list_of_segments.append("\n% define goal states\n")
 for i in range(0, 4):
     for j in range(i + 1, 6):
         for y in range(i + 1, j):
-            list_of_segments.append("green$10_" + str(i) + "." + str(j) +
-                                    " & green$10_" + str(i) + "." + str(y) +
-                                    " & green$10_" + str(y) + "." + str(j) + " |\n")
+            # changed green to red here, since green wins if red has a triangle
+            list_of_segments.append("red$10_" + str(i) + "." + str(j) +
+                                    " & red$10_" + str(i) + "." + str(y) +
+                                    " & red$10_" + str(y) + "." + str(j) + " |\n")
 list_of_segments[-1] = list_of_segments[-1][:-3]  # cut off last part since this is the end of the formula
 # ----------------------------------------------------------------------------------------------------------------------
 segments = ''.join(list_of_segments)
-# TODO join together formula correctly using for all (#) and exists (?)
 # print(segments)
 # writing to file ------------------------------------------------------------------------------------------------------
 with open('init.boole', 'w') as f:
