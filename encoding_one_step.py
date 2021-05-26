03aead38d1026e7d1087f46858537d937fdd350f
FloJoeHD <fpiminger@gmail.com>
2021-05-26 22:52:44 +0200

    added quantifiers as test, with flag to turn on and off Currently formula evaluates to false (but SAT?) TODO: find out why

--- encoding_one_step.py
+++ encoding_one_step.py
@@ -1,16 +1,32 @@
 # debug file to encoding_gen.py
 # TODO: change nested loops to for i, tup in enumerate(list_of_game_edges)
 oneStep = False  # with this, one can switch between one step or two steps
-emptyBoard = False  # with this, one can try a non empty board
+emptyBoard = True  # with this, one can try a non empty board
+writeQuantifiers = True  # use quantifiers if enabled
+nrSteps = 1 if oneStep else 2
 # store game edges in list of tuples
 list_of_game_edges = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5),
                       (1, 2), (1, 3), (1, 4), (1, 5),
                       (2, 3), (2, 4), (2, 5),
                       (3, 4), (3, 5),
                       (4, 5)]
+list_of_segments = []
 # in this test file we don't use quantifiers as we just want to check one step -----------------------------------------
+if writeQuantifiers:
+    list_of_segments.append("% first, quantifiers are written\n")
+    for s in range(0, nrSteps + 1):  # quantifiers always need to be one more than steps to cover wining state
+        for idx, tup in enumerate(list_of_game_edges):
+            if s == 0:
+                list_of_segments.append("? green$0_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+                list_of_segments.append("? red$0_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+            elif s % 2 != 0:  # every odd state, we use an existential quantifier '?'
+                list_of_segments.append("? green$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+                list_of_segments.append("? red$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+            else:  # every even state, we use a for all quantifier '#'
+                list_of_segments.append("# green$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+                list_of_segments.append("# red$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
 # ----------------------------------------------------------------------------------------------------------------------
-list_of_segments = ["% testing if one or two steps work\n"]
+list_of_segments.append("% testing if one or two steps work\n")
 # append init formula --------------------------------------------------------------------------------------------------
 if emptyBoard:
     list_of_segments.append("% init board with all positions to empty\n")
@@ -29,7 +45,7 @@ else:  # this theoretically is not possible as in state 0 there can't usually be
 
 # moves of players in alternating turns --------------------------------------------------------------------------------
 # moves have brackets around them and also implies has brackets eg. x & y & (a -> b)
-nrSteps = 1 if oneStep else 2
+
 list_of_segments.append("% define moves of players\n")
 for s in range(0, nrSteps):  # steps are restricted to 1 here to test if a single step works (or two steps work)
     list_of_segments.append("(\n")
