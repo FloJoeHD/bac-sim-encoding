ce11b973038d9400cc1d1bd3c4d4069bb882523e
FloJoeHD <fpiminger@gmail.com>
2021-08-20 15:36:28 +0200

    added tuple list like in encoding_one_step.py changed some loops to use enumerate of the tuple list changed structure from exists forall to forall exists according to Kfoury and some code cleanup

--- encoding_gen.py
+++ encoding_gen.py
@@ -1,46 +1,40 @@
 # Sim game encoding QBF
-
+# store game edges in list of tuples
+list_of_game_edges = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5),
+                      (1, 2), (1, 3), (1, 4), (1, 5),
+                      (2, 3), (2, 4), (2, 5),
+                      (3, 4), (3, 5),
+                      (4, 5)]
 # at the beginning we want to write the alternating quantifiers to the file --------------------------------------------
-# TODO: change nested loops to enumerate with list of tuples (like in encoding_one_step.py)
 # a single colored line is represented like this: green$0_0.1
 list_of_segments = ["% first, quantifiers are written\n"]
-# step 0
-for i in range(0, 5):  # TODO this can be changed to be in the following iteration
-    for j in range(i + 1, 6):
-        list_of_segments.append("? green$0_" + str(i) + "." + str(j) + "\n")
-        list_of_segments.append("? red$0_" + str(i) + "." + str(j) + "\n")
+for s in range(0, 16):  # run through all states from 0 to 15
 
-for s in range(1, 16):  # TODO: run through all states from 1 to 15
-
-    for i in range(0, 5):
-        for j in range(i + 1, 6):
-            if s % 2 != 0:  # every odd state, we use an existential quantifier '?'
-                list_of_segments.append("? green$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
-                list_of_segments.append("? red$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
-            else:  # every even state, we use a for all quantifier '#'
-                list_of_segments.append("# green$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
-                list_of_segments.append("# red$" + str(s) + "_" + str(i) + "." + str(j) + "\n")
+    for idx, tup in enumerate(list_of_game_edges):
+        if s % 2 != 0:  # every odd state, we use a for all quantifier '#'
+            list_of_segments.append("# green$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+            list_of_segments.append("# red$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+        else:  # every even state, we use an existential quantifier '?'
+            list_of_segments.append("? green$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
+            list_of_segments.append("? red$" + str(s) + "_" + str(tup[0]) + "." + str(tup[1]) + "\n")
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 # append init formula --------------------------------------------------------------------------------------------------
 list_of_segments.append("% init board with all positions to empty\n")
-for i in range(0, 5):
-    for j in range(i + 1, 6):
-        list_of_segments.append("! green$0_" + str(i) + "." + str(j) + " &\n")
-        list_of_segments.append("! red$0_" + str(i) + "." + str(j) + " &\n")
+for idx, tup in enumerate(list_of_game_edges):
+    list_of_segments.append("! green$0_" + str(tup[0]) + "." + str(tup[1]) + " &\n")
+    list_of_segments.append("! red$0_" + str(tup[0]) + "." + str(tup[1]) + " &\n")
 # ----------------------------------------------------------------------------------------------------------------------
 
 # moves of players in alternating turns --------------------------------------------------------------------------------
 # moves have brackets around them and also implies has brackets eg. x & y & (a -> b)
 list_of_segments.append("% define moves of players\n")
 for s in range(0, 15):
-    list_of_segments.append("(\n")
-    if s != 0 and s != 9:  # in the last state we don't need to open another bracket
-        list_of_segments.append("(\n")  # open bracket before '->'
+    list_of_segments.append("(\n(\n")  # enclose one step with all options and open another bracket before & or ->
     for i in range(0, 5):
         for j in range(i + 1, 6):
-            list_of_segments.append("(\n")
+            list_of_segments.append("(\n")  # enclose one option of one step
 
             for k in range(0, 5):
                 for l in range(k + 1, 6):
@@ -60,14 +54,11 @@ for s in range(0, 15):
                                         " & ! green$" + str(s + 1) + "_" + str(i) + "." + str(j) +
                                         " & red$" + str(s + 1) + "_" + str(i) + "." + str(j) + "\n")
             list_of_segments.append(") | ")
-    if s % 2 == 0:
-        if s == 0:  # at the beginning we don't need to close an additional bracket
-            list_of_segments[-1] = list_of_segments[-1][:-2] + ") &\n% next players move\n"
-        else:
-            list_of_segments[-1] = list_of_segments[-1][:-2] + ") &\n% next players move\n"
+    if s % 2 != 0:
+        list_of_segments[-1] = list_of_segments[-1][:-2] + ") &\n% next players move\n"
     else:
-        list_of_segments[-1] = list_of_segments[-1][:-2] + ") ->\n"
-#  list_of_segments[-1] = list_of_segments[-1][:-3] + "&"  # change last element so that the last 3 chars are cut off
+        list_of_segments[-1] = list_of_segments[-1][:-2] + ") ->\n% next players move\n"
+list_of_segments[-1] = list_of_segments[-1][:-21]  # change last element so that the last 21 chars are cut off
 # ----------------------------------------------------------------------------------------------------------------------
 
 # append goal state formula --------------------------------------------------------------------------------------------
@@ -80,9 +71,9 @@ for i in range(0, 4):
             list_of_segments.append("green$15_" + str(i) + "." + str(j) +
                                     " & green$15_" + str(i) + "." + str(y) +
                                     " & green$15_" + str(y) + "." + str(j) + " |\n")
-list_of_segments[-1] = list_of_segments[-1][:-3]  # cut off last part since this is the end of the formula
+list_of_segments[-1] = list_of_segments[-1][:-3]  # cut off last " |\n" since this is the end of the formula
 list_of_segments.append(" )")
-list_of_segments.append(")))))))))))))")  # close all brackets opened by implies, TODO: maybe make this use a counter
+list_of_segments.append(")))))))))))))))")  # close all brackets opened by each state (15)
 # ----------------------------------------------------------------------------------------------------------------------
 segments = ''.join(list_of_segments)
 # print(segments)
